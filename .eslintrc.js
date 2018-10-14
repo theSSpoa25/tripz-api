d312ad88fc0a6a94268b51fa90348c3ac36196fe
egert <egertgjyla1995@gmail.com>
2018-10-14 22:14:53 +0200

    init

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    "parser": "babel-eslint",
+    "extends": "airbnb-base",
+    "rules": {
+        "linebreak-style": ["error", "windows"],
+        "semi": ["error", "never"],
+        "no-template-curly-in-string": "error"
+    }
+};
