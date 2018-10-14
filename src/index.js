263f91c1bfdec2b1bb07bb16a0face02990742e4
egert <egertgjyla1995@gmail.com>
2018-10-14 22:34:28 +0200

    fake data

--- src/index.js
+++ src/index.js
@@ -3,6 +3,21 @@ const express = require('express')
 const app = express()
 const port = 8080
 
-app.get('/', (req, res) => res.send('Hello World'))
+const fakeData = [
+  {
+    "userId": 1,
+    "id": 1,
+    "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+    "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+  },
+  {
+    "userId": 1,
+    "id": 2,
+    "title": "qui est esse",
+    "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
+  },
+]
+
+app.get('/api/fake', (req, res) => res.send(fakeData))
 
 app.listen(port, () => console.log(`Test ${port}!`))
